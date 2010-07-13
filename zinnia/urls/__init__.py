8fc42534764eb95bfc0bb567a90b67a82ce19734
Evgeniy Gulitsyn <evgeniy.gulitsyn@redsolution.ru>
2010-07-13 14:49:26 +0600

    Совместимость с django 1.1(убрал rss)

--- zinnia/urls/__init__.py
+++ zinnia/urls/__init__.py
@@ -3,7 +3,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
                        url(r'^tags/', include('zinnia.urls.tags',)),
-                       url(r'^feeds/', include('zinnia.urls.feeds')),
+#                       url(r'^feeds/', include('zinnia.urls.feeds')),
                        url(r'^authors/', include('zinnia.urls.authors')),
                        url(r'^categories/', include('zinnia.urls.categories')),
                        url(r'^search/', include('zinnia.urls.search')),

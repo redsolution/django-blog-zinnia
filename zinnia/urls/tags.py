2d03ccdcb8f214abcaf6c8aee9950e81e673dc11
Alexey Kuligin <alexey.kuligin@redsolution.ru>
2010-08-18 12:34:03 +0600

    add white-space

--- zinnia/urls/tags.py
+++ zinnia/urls/tags.py
@@ -10,12 +10,12 @@ tag_conf = {'queryset': tags_published(),
             'template_name': 'zinnia/tag_list.html'}
 
 tag_conf_entry = {'queryset_or_model': Entry.published.all(),
-                  'paginate_by': PAGINATION,}
+                  'paginate_by': PAGINATION, }
 
 urlpatterns = patterns('zinnia.views.tags',
                        url(r'^$', 'tag_list',
                            tag_conf, name='zinnia_tag_list'),
-                       url(r'^(?P<tag>[-\w]+)/$', 'tag_detail',
+                       url(r'^(?P<tag>[-\s\w]+)/$', 'tag_detail',
                            tag_conf_entry, name='zinnia_tag_detail'),
                        url(r'^(?P<tag>[-\w]+)/page/(?P<page>\d+)/$',
                            'tag_detail', tag_conf_entry,
